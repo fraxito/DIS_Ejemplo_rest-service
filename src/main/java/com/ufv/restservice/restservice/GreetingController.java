83b3631639f59d757b680ece73b8c37c909e4f1f
Jorge Cisneros <fraxito@gmail.com>
2022-11-12 23:33:38 +0100

    Final de la parte 2

--- src/main/java/com/ufv/restservice/restservice/GreetingController.java
+++ src/main/java/com/ufv/restservice/restservice/GreetingController.java
@@ -1,8 +1,11 @@
 package com.ufv.restservice.restservice;
 
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +19,16 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
+
+    @GetMapping("/users")
+    public ArrayList<User> users(){
+        JsonReader reader = new JsonReader();
+        ArrayList<User> userList = reader.readJsonFile("./src/main/resources/users.json");
+        return userList;
+    }
+
+  //  @GetMapping("/users/{name}")
+  //  public ResponseEntity<User> getByName(@PathVariable String name){
+
+  //  }
 }
---
+++ src/main/java/com/ufv/restservice/restservice/JsonReader.java
@@ -0,0 +1,30 @@
+package com.ufv.restservice.restservice;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class JsonReader {
+
+    public ArrayList<User> readJsonFile(String fichero){
+        try {
+            //lee el fichero que le pasemos y lo carga en un reader
+            Reader reader = Files.newBufferedReader(Paths.get(fichero));
+            // convierte el array JSON a un arraylist de users
+            ArrayList<User> users = new Gson().fromJson(reader, new TypeToken<ArrayList<User>>() {}.getType());
+
+            users.forEach(System.out::println);// imprime los users
+            reader.close();// close reader
+            return users;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ArrayList<>(); //si no ha leido nada, devuelve un array vacio
+        }
+
+    }
+}
